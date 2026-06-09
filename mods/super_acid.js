889a9e41d01cca187dd46c60aa8729ec37bf9689
LunarSeeker <109869157+LunarSeeker@users.noreply.github.com>
2026-06-08 20:15:49 -0400

    Update super_acid.js

--- mods/super_acid.js
+++ mods/super_acid.js
@@ -5,25 +5,26 @@ elements.super_acid = {
 	behavior: behaviors.LIQUID,
 	state: "liquid",
 	category: "liquids",
-	density: 3000,
-	hardness: 1,
-	breakInto: ["hydrogen", "acid"],
+	density: 300,
+	breakInto: ["hydrogen", "water"],
 	tick: function (pixel) {
 		for (i = 0; i < adjacentCoords.length; i++) {
-			if (Math.random() < 0.5) {
-				var checkPosX = pixel.x + adjacentCoords[i][0];
-				var checkPosY = pixel.y + adjacentCoords[i][1];
-				if (!isEmpty(checkPosX, checkPosY, true)) {
-					var newElement = pixelMap[checkPosX][checkPosY].element;
-					if (!acidBlacklist.includes(newElement)) {
-						if (typeof (pixel[newElement]) === "undefined") {
-							pixel[newElement] = 0;
-						};
-						pixel[newElement]++;
-						deletePixel(checkPosX, checkPosY);
+			//if (Math.random() < 0.5) {
+			var checkPosX = pixel.x + adjacentCoords[i][0];
+			var checkPosY = pixel.y + adjacentCoords[i][1];
+			if (!isEmpty(checkPosX, checkPosY, true)) {
+				var newElement = pixelMap[checkPosX][checkPosY].element;
+				if (!acidBlacklist.includes(newElement)) {
+					if (typeof (pixel[newElement]) === "undefined") {
+						pixel[newElement] = 0;
 					};
+					pixel[newElement]++;
+					deletePixel(checkPosX, checkPosY);
 				};
 			};
+			//};
 		};
 	},
 };
+
+elements.bless.reactions.super_acid = { elem2: null }
