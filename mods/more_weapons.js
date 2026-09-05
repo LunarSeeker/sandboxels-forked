0ce3dd8986193a14a7781f54eae342c9b974aadd
LunarSeeker <109869157+LunarSeeker@users.noreply.github.com>
2026-09-05 08:42:23 -0400

    Update more_weapons.js

--- mods/more_weapons.js
+++ mods/more_weapons.js
@@ -1,14 +1,24 @@
 elements.subzero_bomb = {
-    behavior: [
-        "XX|EX:10>cold_fire|XX",
-        "XX|XX|XX",
-        "M2|M1 AND EX:60>cold_fire,cold_fire,cold_fire,cold_fire,cold_fire,cold_fire|M2"
-    ],
     category: "weapons",
     color: "#00d5ff",
     cooldown: defaultCooldown,
     darkText: true,
     density: 1500,
     excludeRandom: true,
-    state: "solid"
+    state: "solid",
+    tick: function (pixel) {
+        if (pixel.start === pixelTicks) { return }
+        if (!tryMove(pixel, pixel.x, pixel.y + 1)) {
+            if (outOfBounds(pixel.x, pixel.y + 1) || (pixelMap[pixel.x][pixel.y + 1].element !== "subzero_bomb")) {
+                for (i = 0; i < currentPixels.length; i++) {
+                    var newPixel = currentPixels[i]
+                    if (newPixel.temp > -273) {
+                        newPixel.temp = -273
+                    }
+                }
+                explodeAt(pixel.x, pixel.y + 1, 20, "flash")
+            }
+        }
+        doDefaults(pixel)
+    }
 }
