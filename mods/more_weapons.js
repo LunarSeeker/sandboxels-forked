44403fa8bc85f8f241804a02ca9d2a2f9159ad95
LunarSeeker <109869157+LunarSeeker@users.noreply.github.com>
2026-09-04 20:10:12 -0400

    Add files via upload

---
+++ mods/more_weapons.js
@@ -0,0 +1,14 @@
+elements.subzero_bomb = {
+    behavior: [
+        "XX|EX:10>cold_fire|XX",
+        "XX|XX|XX",
+        "M2|M1 AND EX:60>cold_fire,cold_fire,cold_fire,cold_fire,cold_fire,cold_fire|M2"
+    ],
+    category: "weapons",
+    color: "#00d5ff",
+    cooldown: defaultCooldown,
+    darkText: true,
+    density: 1500,
+    excludeRandom: true,
+    state: "solid"
+}
