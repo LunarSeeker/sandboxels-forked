83be652d9b925b0b124916a67eb08b8a1a3402e4
LunarSeeker <109869157+LunarSeeker@users.noreply.github.com>
2026-09-05 12:55:47 -0400

    Update more_weapons.js

--- mods/more_weapons.js
+++ mods/more_weapons.js
@@ -1,3 +1,8 @@
+function randomChoice(array) {
+    if (array.length === 0) { throw new Error(`The array ${array} is empty`) };
+    return array[(randomIntegerFromZeroToValue(array.length - 1))]
+};
+
 elements.subzero_bomb = {
     category: "weapons",
     color: "#00d5ff",
@@ -39,6 +44,28 @@ elements.ultrahot_bomb = {
                     if (newPixel.temp < 999) {
                         newPixel.temp += 999
                     }
+                }
+                explodeAt(pixel.x, pixel.y + 1, 20, "flash")
+            }
+        }
+        doDefaults(pixel)
+    }
+}
+
+elements.color_bomb = {
+    category: "weapons",
+    color: "#ff0000",
+    cooldown: defaultCooldown,
+    density: 1500,
+    excludeRandom: true,
+    state: "solid",
+    tick: function (pixel) {
+        if (pixel.start === pixelTicks) { return }
+        if (!tryMove(pixel, pixel.x, pixel.y + 1)) {
+            if (outOfBounds(pixel.x, pixel.y + 1) || (pixelMap[pixel.x][pixel.y + 1].element !== "color_bomb")) {
+                for (i = 0; i < currentPixels.length; i++) {
+                    var newPixel = currentPixels[i]
+                    newPixel.color = randomChoice(["#ff0000", "#ff8800", "#ffff00", "#00ff00", "#00ffff", "#0000ff", "#ff00ff"])
                 }
                 explodeAt(pixel.x, pixel.y + 1, 20, "flash")
             }
