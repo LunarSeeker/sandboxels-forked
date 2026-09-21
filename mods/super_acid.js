dc3358c810d5461e1967edf192744a2a097adff6
LunarSeeker <109869157+LunarSeeker@users.noreply.github.com>
2026-09-20 21:05:00 -0400

    Add files via upload

---
+++ mods/super_acid.js
@@ -0,0 +1,51 @@
+acidBlacklist = [
+	"battery",
+	"black_hole",
+	"bless",
+	"border",
+	"gray_goo",
+	"ichor",
+	"image",
+	"pipe_wall",
+	"pipe",
+	"portal_in",
+	"portal_out",
+	"scp_447",
+	"scp_682",
+	"super_acid",
+	"time",
+	"void",
+	"wall",
+	"warp",
+	"wire",
+]
+
+elements.super_acid = {
+	behavior: behaviors.LIQUID,
+	breakInto: ["hydrogen", "water"],
+	category: "liquids",
+	color: "#77b300",
+	density: 300,
+	excludeRandom: true,
+	state: "liquid",
+	tick: function (pixel) {
+		for (i = 0; i < adjacentCoords.length; i++) {
+			//if (Math.random() < 0.5) {
+			var checkPosX = pixel.x + adjacentCoords[i][0]
+			var checkPosY = pixel.y + adjacentCoords[i][1]
+			if (!isEmpty(checkPosX, checkPosY, true)) {
+				var newElement = pixelMap[checkPosX][checkPosY].element
+				if (!acidBlacklist.includes(newElement)) {
+					if (typeof (pixel[newElement]) === "undefined") {
+						pixel[newElement] = 0
+					};
+					pixel[newElement]++
+					deletePixel(checkPosX, checkPosY)
+				};
+			};
+			//};
+		};
+	},
+}
+
+elements.bless.reactions.super_acid = { elem2: null }
